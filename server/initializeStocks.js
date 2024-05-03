bd20126bd8f81afb0945b22563d606e449f3fc8e
Kevin Khuu <notkevinkhuu@gmail.com>
2024-05-03 02:10:48 -0700

    Initialized price based on elo

--- server/initializeStocks.js
+++ server/initializeStocks.js
@@ -28,14 +28,22 @@ const teams = [
   "G2",
 ];
 
+const S0 = 1000; // Starting Elo
+
 async function initializeStocks() {
   for (const symbol of teams) {
-    const newStock = await createStock(symbol, 1000, 0, 800);
+    const newStock = await createStock(symbol, 1000, 0, S0);
   }
 }
 
 async function initializeElo() {
   try {
+    const allStocks = await CurrentStockPrice.find({});
+    for (const stocks of allStocks) {
+      stocks.elo = S0;
+      await stocks.save();
+    }
+
     const matchData = await getAndFilterMatches();
 
     for (let i = matchData.length - 1; i >= 0; i--) {
@@ -65,9 +73,20 @@ async function initializeElo() {
 
       // https://stanislav-stankovic.medium.com/elo-rating-system-6196cc59941e#:~:text=After%20each%20match%2C%20the%20Elo,the%20match%20and%20SA%20is
       const c = 400;
-      const K = 32;
-      const L = 32;
-      const V = 10;
+      const K = 90;
+      const L = 50;
+      let V = 16;
+
+      if (match["match_series"].includes("Semifinals")) {
+        V *= 1.5;
+      } else if (
+        match["match_series"].includes("Upper Final") ||
+        match["match_series"].includes("Lower Final")
+      ) {
+        V *= 4;
+      } else if (match["match_series"].includes("Grand Final")) {
+        V *= 8;
+      }
 
       let Ra = 1000;
       let Rb = 1000;
@@ -95,17 +114,27 @@ async function initializeElo() {
       const newRb = Rb + K * (Sb - Eb) + L * Pb + Sb * V;
 
       if (team1Stock) {
-        team1Stock.elo = newRa;
+        team1Stock.elo = Math.round(newRa);
+        team1Stock.price =
+          Math.round(((team1Stock.price * newRa) / Ra) * 100) / 100; // ** EXPERIMENT **
         await team1Stock.save();
       }
 
       if (team2Stock) {
-        team2Stock.elo = newRb;
+        team2Stock.elo = Math.round(newRb);
+        team2Stock.price =
+          Math.round(((team2Stock.price * newRb) / Rb) * 100) / 100; // ** EXPERIMENT **
         await team2Stock.save();
       }
     }
 
-    console.log("DONE!");
+    console.log("DONE");
+
+    const updatedStocks = await CurrentStockPrice.find({}).sort({ elo: -1 });
+
+    for (const stock of updatedStocks) {
+      console.log(stock.symbol + ": " + stock.elo);
+    }
 
     return;
   } catch (error) {
