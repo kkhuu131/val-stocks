4ca18aa149a99ada2c367e3f9994052ad75ca4c1
Kevin Khuu <notkevinkhuu@gmail.com>
2024-05-20 20:50:44 -0700

    Added region filter

--- src/components/StockDisplayContainer.jsx
+++ src/components/StockDisplayContainer.jsx
@@ -2,12 +2,38 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import StockDisplayRow from "./StockDisplayRow";
 import teamData from "../teamMappings.json";
-import { Box, Flex, Input } from "@chakra-ui/react";
+import { Box, Grid, Flex, Input, Select } from "@chakra-ui/react";
 
 const StockDisplayContainer = () => {
   const [stocks, setStocks] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
   const [filteredStocks, setFilteredStocks] = useState([]);
+  const [selectedRegion, setSelectedRegion] = useState("All");
+  const regions = {
+    Americas: [
+      "United States",
+      "Chile",
+      "Brazil",
+      "Argentina"
+    ],
+    EMEA: [
+      "Europe",
+      "Turkey",
+      "France"
+    ],
+    APAC: [
+      "Japan",
+      "South Korea",
+      "Singapore",
+      "India",
+      "Indonesia",
+      "Thailand",
+      "Philippines"
+    ],
+    CN: [
+      "China"
+    ]
+  }
 
   useEffect(() => {
     const fetchStocks = async () => {
@@ -25,18 +51,31 @@ const StockDisplayContainer = () => {
   });
 
   useEffect(() => {
-    const filtered = stocks.filter((stock) =>
-      stock.symbol.toLowerCase().includes(searchQuery.toLowerCase()) || (teamData.teamBySymbolMap[stock.symbol] && teamData.teamBySymbolMap[stock.symbol].name.toLowerCase().includes(searchQuery.toLowerCase()))
-    );
+    const filtered = stocks.filter((stock) => {
+      const matchesSearchQuery = stock.symbol.toLowerCase().includes(searchQuery.toLowerCase()) || (teamData.teamBySymbolMap[stock.symbol] && teamData.teamBySymbolMap[stock.symbol].name.toLowerCase().includes(searchQuery.toLowerCase()))
+      const matchesRegion = selectedRegion === "All" || regions[selectedRegion].includes(teamData.teamBySymbolMap[stock.symbol].country);
+      return matchesSearchQuery && matchesRegion;
+  });
 
     setFilteredStocks(filtered);
   }, [searchQuery, stocks]);
 
   return (
     <Box m={1} mx="auto" maxW="70%" minW="800px">
-      <Flex justifyContent="center" alignItems="center" w="100%" mb="4">
-        <Input color="white" fontSize="24" h="50px" placeholder="Search stocks..." value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)}/>
-      </Flex>
+      <Grid gridTemplateColumns="4fr 2fr 3fr">
+        <Flex justifyContent="center" alignItems="center" w="100%" mb="4">
+          <Input borderColor="grayAlpha.500" color="white" fontSize="24" h="50px" placeholder="Search stocks..." value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)}/>
+        </Flex>
+        <Flex>
+          <Select color="white" backgroundColor="black" fontSize="24" h="50px" borderColor="grayAlpha.500" value={selectedRegion} onChange={(e) => setSelectedRegion(e.target.value)}>
+            <option value="All" style={{ color: 'black'}}>All Regions</option>
+            <option value="Americas" style={{ color: 'black'}}>Americas</option>
+            <option value="EMEA" style={{ color: 'black'}}>EMEA</option>
+            <option value="APAC" style={{ color: 'black'}}>Pacific</option>
+            <option value="CN" style={{ color: 'black'}}>China</option>
+          </Select>
+        </Flex>
+      </Grid>
       {filteredStocks.length > 0 && filteredStocks.map((item, index) => {
         return <StockDisplayRow key={index} stock={item} />;
       })}
